085af6cba45213dc0c757888b38327ad180cd62d
kdoul06 <kdoul06@gmail.com>
2017-01-20 04:48:32 +0200

    .

--- app/src/main/java/com/example/accel/whiterabbit3/activities/MainActivity.java
+++ app/src/main/java/com/example/accel/whiterabbit3/activities/MainActivity.java
@@ -1,31 +1,71 @@
 package com.example.accel.whiterabbit3.activities;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.accel.whiterabbit3.R;
 import com.example.accel.whiterabbit3.models.Poi;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    EditText emailTxt;
+    TextView lonTxt, latTxt, catTxt;
+    public int text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("poi");
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("poi");
 
+//        emailTxt = (EditText) findViewById(R.id.userEmailTxt);
+        lonTxt = (TextView) findViewById(R.id.lonTxt);
+        latTxt = (TextView) findViewById(R.id.latTxt);
+        catTxt = (TextView) findViewById(R.id.catTxt);
+
+    }
+
+    public void onReadBtnClick(View view){
+
+    }
+
+    public void onWritePoiBtnClick(View view){
+        // Write POI to the database
         String key = myRef.child("poi").push().getKey();
 
-        Poi poi = new Poi( "1",2,3,"1","cat_" + key);
-        myRef.child(key).setValue(poi);
+                Poi poi = new Poi("1",Double.parseDouble(lonTxt.getText().toString())
+                             ,Double.parseDouble(latTxt.getText().toString())
+                             ,"1",catTxt.getText().toString());
 
-        key = myRef.child("poi").push().getKey();
-        poi  = new Poi( "1",2,3,"1","cat_" + key);
-        myRef.child(key).setValue(poi);
+        myRef.child(key).setValue(poi).isSuccessful();
+
     }
+
+    public void onWriteUserBtnClick(View view){
+
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_LONG;
+
+
+        Toast toast = Toast.makeText(context, lonTxt.getText().toString(),duration);
+        toast.show();
+
+
+
+
+
+    }
+
 }
--- app/src/main/java/com/example/accel/whiterabbit3/models/Poi.java
+++ app/src/main/java/com/example/accel/whiterabbit3/models/Poi.java
@@ -8,16 +8,16 @@ public class Poi {
 
 
     public String userId;
-    public double lat;
     public double lon;
+    public double lat;
     public String catId;
     public String catDescr;
 
 
-    public Poi(String userId, double lat, double lon, String catId, String catDescr) {
+    public Poi(String userId, double lon, double lat, String catId, String catDescr) {
         this.userId = userId;
-        this.lat = lat;
         this.lon = lon;
+        this.lat = lat;
         this.catId = catId;
         this.catDescr = catDescr;
     }
